7f4121aacbde9126eb79e7f0e49e055712eabc0b
Nikolay Vasilev <lexuspexus@yandex.ru>
2022-12-13 17:04:43 +0300

    Change Main and BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int cost = 10_853;
-        int miles = service.calculate(cost);
+        int price = 10_853;
+        int miles = service.calculate(price);
         System.out.println("Количество миль - " + miles);
     }
 }
